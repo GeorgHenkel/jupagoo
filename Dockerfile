ad89a74100391ed0a37342de1d297c08ff00fbb9
Georg Henkel <g.henkel@cgh-solutions.de>
2018-01-30 07:28:24 +0100

    add datasource and docker with hsqldb

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,9 @@
 FROM airhacks/glassfish
+
 COPY ./target/jupagoo.war ${DEPLOYMENT_DIR}
+COPY ./src/main/lib/hsqldb-2.4.jar ${GLASSFISH_HOME}/domains/domain1/lib/ext
+
+ADD hsqldb-2.3.2.jar /opt/app/extlib/hsqldb-2.3.2.jar
+
+ADD bin/configure-glassfish.sh /opt/app/bin/configure-glassfish.sh
+RUN /opt/app/bin/initialize-glassfish.sh
--- src/main/java/de/georghenkel/jupagoo/interfaces/jsf/storage/StorageProviderController.java
+++ src/main/java/de/georghenkel/jupagoo/interfaces/jsf/storage/StorageProviderController.java
@@ -1,7 +1,9 @@
 package de.georghenkel.jupagoo.interfaces.jsf.storage;
 
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -10,15 +12,29 @@ import de.georghenkel.jupagoo.domain.storage.repository.StorageRepository;
 
 @Named
 @ViewScoped
-public class StorageProviderController {
+public class StorageProviderController implements Serializable {
+  private static final long serialVersionUID = 1L;
+
   @Inject
   private StorageRepository repository;
 
-  public List<StorageConnection> getAvailableProvider() {
-    return repository.loadStorageConnections();
+  private List<StorageConnection> managedConnections;
+
+  @PostConstruct
+  public void init() {
+    managedConnections = repository.loadStorageConnections();
   }
 
-  public List<StorageProvider> getManagedStorageProvider() {
-    return Arrays.asList(StorageProvider.DROPBOX);
+  public List<StorageConnection> getManagedConnections() {
+    return managedConnections;
+  }
+
+  public List<StorageProvider> getAvailableProviders() {
+    return Arrays.asList(StorageProvider.DROPBOX, StorageProvider.GOOGLE_DRIVE,
+        StorageProvider.BOX);
+  }
+
+  public boolean isManaged(final StorageProvider provider) {
+    return managedConnections.stream().anyMatch(mc -> mc.getProvider() == provider.getType());
   }
 }
